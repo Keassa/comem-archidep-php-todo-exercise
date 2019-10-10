bd4975e84b089c3cf0cd93847c725b4b39de3df4
Leonard Michiels <leonard.michiels@heig-vd.ch>
2019-10-10 14:48:59 +0200

    Changed BASE_URL to the correct redirection path

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-define('BASE_URL', '/');
+define('BASE_URL', '/comem-archidep-php-todo-exercise/');
 define('DB_USER', 'todolist');
-define('DB_PASS', 'chAngeMeN0w!');
+define('DB_PASS', 'psql');
 define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
@@ -70,7 +70,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'select * from todo order by id asc'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
